63620b199bf5dbadd83af6ef87fdeb7af3c53422
Bennett <bennett@yobennett.com>
2015-02-07 12:40:40 -0800

    add weighted qu; add toString

--- src/Percolation.java
+++ src/Percolation.java
@@ -3,8 +3,9 @@ public class Percolation {
 	private int gridSize;
 	private boolean[][] grid;
 	private int nodesSize;
-	private int virtualTopIndex;
-	private int virtualBottomIndex;
+	private WeightedQuickUnionUF nodes;
+	private int virtualTopNodeIndex;
+	private int virtualBottomNodeIndex;
 
 	// constructor
 	public Percolation(int gridSize) {
@@ -16,39 +17,51 @@ public class Percolation {
 
 		this.gridSize = gridSize;
 
-		// initialize grid
-		this.grid = new boolean[gridSize][gridSize];
+		// initialize grid, ignore first row and column
+		this.grid = new boolean[gridSize + 1][gridSize + 1];
 
+		// initialize nodes for weighted quick-union, union-find
 		this.nodesSize = (int) Math.pow(gridSize, 2) + 2;
-		this.virtualTopIndex = 0;
-		this.virtualBottomIndex = nodesSize - 1;
+		this.virtualTopNodeIndex = 0;
+		this.virtualBottomNodeIndex = nodesSize - 1;
+		this.nodes = new WeightedQuickUnionUF(nodesSize);
 	}
 
 	// open site (row i, column j) if it is not open already
 	public void open(int i, int j) throws IndexOutOfBoundsException {
-		if (!isWithinGridBounds(i, j)) {
-			throw new IndexOutOfBoundsException("grid coordinates not within accepted range (i=" + i + ", j=" + j + "+)");
+		checkWithinGridBounds(i, j);
+
+		// already open
+		if (isOpen(i, j)) {
+			return;
 		}
+
 	}
 
 	// is site (row i, column j) open?
 	public boolean isOpen(int i, int j) throws IndexOutOfBoundsException {
-		if (!isWithinGridBounds(i, j)) {
-			throw new IndexOutOfBoundsException("grid coordinates not within accepted range (i=" + i + ", j=" + j + "+)");
-		}
+		checkWithinGridBounds(i, j);
 		return grid[i][j];
 	}
 
 	// is site (row i, column j) full?
 	public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
+		checkWithinGridBounds(i, j);
+		return false;
+	}
+
+	private void checkWithinGridBounds(int i, int j) {
 		if (!isWithinGridBounds(i, j)) {
 			throw new IndexOutOfBoundsException("grid coordinates not within accepted range (i=" + i + ", j=" + j + "+)");
 		}
-		return false;
 	}
 
 	private boolean isWithinGridBounds(int i, int j) {
 		return i > 0 && i <= gridSize && j > 0 && j <= gridSize;
+	}
+
+	private int xyTo1D(int x, int y) {
+		return ((x - 1) * gridSize) + y;
 	}
 
 	// does the system percolate?
@@ -56,10 +69,25 @@ public class Percolation {
 		return false;
 	}
 
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 1; i <= gridSize; i++) {
+			for (int j = 1; j <= gridSize; j++) {
+				if (isOpen(i, j)) {
+					builder.append("O ");
+				} else {
+					builder.append("■ ");
+				}
+			}
+			builder.append("\n");
+		}
+		return builder.toString();
+	}
+
 	// test client (optional)
 	public static void main(String[] args) {
 		Percolation p = new Percolation(3);
-		return;
+		System.out.println(p);
 	}
 
 }
