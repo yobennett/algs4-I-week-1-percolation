26f7cbe9e698310467091935a141d88bdd293069
Bennett <bennett@yobennett.com>
2015-02-07 23:37:03 -0800

    experiments with mean

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,8 +1,11 @@
 public class PercolationStats {
 
+	private int gridSize;
+	private int numExperiments;
+	private double[] experimentResults;
+
 	// perform T independent experiments on an N-by-N grid
 	public PercolationStats(int n, int t) {
-
 		if (n <= 0) {
 			throw new IllegalArgumentException("must be positive number for grid size");
 		}
@@ -11,11 +14,37 @@ public class PercolationStats {
 			throw new IllegalArgumentException("must be positive number of experiments to run");
 		}
 
+		this.gridSize = n;
+		this.numExperiments = t;
+		this.experimentResults = new double[numExperiments];
+		runExperiments();
+	}
+
+	private void runExperiments() {
+		int i = 0;
+		while (i < numExperiments) {
+			experimentResults[i] = runExperiment();
+			i++;
+		}
+	}
+
+	private double runExperiment() {
+		Percolation percolation = new Percolation(gridSize);
+		int p, q, openedSites = 0;
+		while (!percolation.percolates()) {
+			p = StdRandom.uniform(1, gridSize + 1);
+			q = StdRandom.uniform(1, gridSize + 1);
+			if (!percolation.isOpen(p, q)) {
+				percolation.open(p, q);
+				openedSites++;
+			}
+		}
+		return openedSites / Math.pow(gridSize, 2);
 	}
 
 	// sample mean of percolation threshold
 	public double mean() {
-		return 0.0;
+		return StdStats.mean(experimentResults);
 	}
 
 	// sample standard deviation of percolation threshold
